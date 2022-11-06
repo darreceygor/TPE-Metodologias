d735e302cc8816da5042de7e8c2ee18fbdee82f5
Luciano Olmedo <luciano7595@outlook.com>
2022-11-06 20:59:54 -0300

    Se agregan funciones de control

--- controller/controller.php
+++ controller/controller.php
@@ -5,12 +5,12 @@ require_once 'view/portal.php';
 class Controller {
 
  private $view;
-
+ private $model;
 
  public function __construct()
     {
         $this->view = new View(); 
-        
+        $this->model = new Model();
     }
 
 
@@ -27,6 +27,30 @@ function showHome()   {
 function showPaciente()   {
      
     $this->view->renderHome();  
+}
+
+function showTurnos()   {
+    $this->view->renderTurnos();  
+}
+
+
+function showAbout()   {     
+   $this->view->renderAbout();  
+}
+
+function showRegistros($filtro)   {
+ 
+    $turnos = $this->model->getTurnos();
+    $this->view->renderRegistros($turnos, $filtro);  
+}
+
+
+function eliminar($id)  //Muestra el detalle.
+{
+ //var_dump($idTurno);
+ 
+   $this->model->deleteTurno($id);    
+   $this->view->refreshTurnos();
 }
 
 }
