ebfe0e40b14079387895f650fc1324bdd2ff6115
Sebastian Rosende Bricka <peperosende1515@gmail.com>
2022-11-06 10:39:25 -0300

    "Subo imagenes"

--- controller/controller.php
+++ controller/controller.php
@@ -1,16 +1,16 @@
 <?php
 require_once 'view/portal.php';
-require_once 'model/turnos.php';
+
 
 class Controller {
 
  private $view;
- private $model;
+
 
  public function __construct()
     {
         $this->view = new View(); 
-        $this->model = new Model();  
+        
     }
 
 
--- router.php
+++ router.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once "controller/controller.php";
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
 if (!empty($_GET['action'])) {
