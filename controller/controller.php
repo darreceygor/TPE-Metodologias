fe1fc22c45728ffcc96d63a9d49d2255529e0a9a
jtobal <ziba.tobal@gmail.com>
2022-11-08 10:08:13 -0300

    se corrje controller

--- controller/controller.php
+++ controller/controller.php
@@ -4,54 +4,52 @@ require_once 'model/turnos.php';
 
 class Controller {
 
- private $view;
- private $model;
+    private $view;
+    private $model;
 
- public function __construct()
-    {
-        $this->view = new View(); 
-        $this->model = new Model();
+    public function __construct()
+        {
+            $this->view = new View(); 
+            $this->model = new Model();
+        }
+
+    function showHome()   {
+        
+        $this->view->renderHome();  
+    }
+
+    function showLogin()   {
+        
+        $this->view->renderLogin();  
+    }
+
+    function showPaciente()   {
+        
+        $this->view->renderHome();  
+    }
+
+    function showTurnos()   {
+        $this->view->renderTurnos();  
     }
 
 
-function showHome()   {
-     
-     $this->view->renderHome();  
- }
-
- function showLogin()   {
-     
-    $this->view->renderLogin();  
-}
-
-function showPaciente()   {
-     
-    $this->view->renderHome();  
-}
-
-function showTurnos()   {
-    $this->view->renderTurnos();  
-}
+    function showAbout()   {     
+    $this->view->renderAbout();  
+    }
 
 
-function showAbout()   {     
-   $this->view->renderAbout();  
-}
+    function showRegistros($filtro)   {
+    
+        $turnos = $this->model->getTurnos($filtro);
+        $this->view->renderRegistros($turnos);  
+    }
 
 
-function showRegistros($filtro)   {
-  
-    $turnos = $this->model->getTurnos($filtro);
-    $this->view->renderRegistros($turnos);  
-}
-
-
-function eliminar($id)  //Muestra el detalle.
-{
- //var_dump($idTurno);
- 
-   $this->model->deleteTurno($id);    
-   $this->view->refreshTurnos();
-}
-
+    function eliminar($id)  //Muestra el detalle.
+    {
+    //var_dump($idTurno);
+    
+    $this->model->deleteTurno($id);    
+    $this->view->refreshTurnos();
+    }
 }
