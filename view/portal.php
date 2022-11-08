7a9a0792369b1197d8406d3bd9bad674a6a4e779
jtobal <ziba.tobal@gmail.com>
2022-11-08 11:24:03 -0300

    fix de views

--- view/portal.php
+++ view/portal.php
@@ -2,35 +2,29 @@
 
 require_once 'smarty/libs/Smarty.class.php';
 
-
 class View
 {
 
-    private $smarty;
+  private $smarty;
 
-    function __construct()
-    {
-        $this->smarty = new Smarty();
+  function __construct()
+    {$this->smarty = new Smarty();
     }
 
     public function renderHome()
-    {
-      $this->smarty->display('templates/home.tpl');
+    {$this->smarty->display('templates/home.tpl');
     }
 
     public function renderLogin()
-    {
-      $this->smarty->display('templates/login.tpl');
-     
+    {$this->smarty->display('templates/login.tpl');
     }
+
     public function renderAbout()
-    {
-      $this->smarty->display('templates/about.tpl');
+    {$this->smarty->display('templates/about.tpl');
     }
 
     public function renderTurnos()
-    {    
-     $this->smarty->display('templates/turnos.tpl');
+    {$this->smarty->display('templates/turnos.tpl');
     }
 
     public function renderRegistros($turnos)
@@ -40,9 +34,6 @@ class View
     }
 
     public function refreshTurnos()
-    {
-        header('Location: ' . BASE_URL . 'turnos');
+    {header('Location: ' . BASE_URL . 'turnos');
     }
-  
-
 }
