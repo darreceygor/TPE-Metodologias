9ff5e88e16ee316bbeba477dc20f7069fcdcb887
jtobal <ziba.tobal@gmail.com>
2022-11-24 20:04:33 -0300

    Correcion de consulta WARMUP-58

--- model/turnos.php
+++ model/turnos.php
@@ -10,7 +10,6 @@ class Model
         $this->db_turnos = new PDO('mysql:host=localhost;' . 'dbname=db_turnofacil;charset=utf8', 'root', '');
     }
     
-
     function deleteTurno($id)
     {
         $query = $this->db_turnos->prepare('DELETE FROM turnos WHERE id_turno= ?');
