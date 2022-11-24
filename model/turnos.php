781cdcf940236fe7b314bc17b3bfb6df425eb3e6
jtobal <ziba.tobal@gmail.com>
2022-11-24 19:49:57 -0300

    Se crea consulta en base de datos WORMUP-58

--- model/turnos.php
+++ model/turnos.php
@@ -10,18 +10,18 @@ class Model
         $this->db_turnos = new PDO('mysql:host=localhost;' . 'dbname=db_turnofacil;charset=utf8', 'root', '');
     }
     
+
+    function deleteTurno($id)
+    {
+        $query = $this->db_turnos->prepare('DELETE FROM turnos WHERE id_turno= ?');
+        $query->execute([$id]);
+    }
+
     function getTurnos($medico)
     {
         $query = $this->db_turnos->prepare('SELECT * FROM turnos WHERE medico=?');
         $query->execute([$medico]);
         $turnos= $query->fetchAll(PDO::FETCH_OBJ);
         return $turnos;
-    }
-
-
-    function deleteTurno($id)
-    {
-        $query = $this->db_turnos->prepare('DELETE FROM turnos WHERE id_turno= ?');
-        $query->execute([$id]);
     }
 }
